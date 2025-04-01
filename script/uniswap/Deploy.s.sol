88423cdc330eceaf215982e3e11bd8586daa17b6
Cuong Tran <tranquoccuong@outlook.com.vn>
2025-04-01 17:55:34 +0700

    feat: deploy experimental hook

--- script/uniswap/Deploy.s.sol
+++ script/uniswap/Deploy.s.sol
@@ -10,7 +10,7 @@ contract DeployScript is BaseUniswapScript {
     // Follow the instructions in
     // https://github.com/KyberNetwork/ks-exclusive-liquidity-sc/?tab=readme-ov-file#deploy
     // to find the suitable salt
-    bytes32 salt = 0x445a84c05d0edc98679a0dc3f9ecc0c33eab94f5b5448daa9b4e400000ca69ca;
+    bytes32 salt = 0xa67964bec72554b7533d1de697007f88deab31e98a72d1ee444ae00000e56f31;
     bytes memory bytecode = abi.encodePacked(
       type(UniswapV4KEMHook).creationCode,
       abi.encode(
